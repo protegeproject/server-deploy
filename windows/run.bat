b7fdf5ce3fffac85823e45595c645c3a7f5fc635
Timothy Redmond <tredmond@stanford.edu>
2012-11-07 18:04:33 +0000

    moving this over to the mac vm's

--- windows/run.bat
+++ windows/run.bat
@@ -1,26 +1,17 @@
 REM User configurable variables
 
-
-
 set hostname=localhost
-
 set memory=700M
-
 set user=Protege
 set install.dir=C:\Program Files\Protege OWL Server
-
-
 
 REM End of user configurable variables section
 
 REM Keep configuring if you like but this should be less likely
 
-
 REM 
 set classpath='%install.dir%\lib\felix.jar;%install.dir%\lib\ProtegeLauncher.jar'
-
 set classpath=lib\felix.jar;lib\ProtegeLauncher.jar
-
 
 set command=java.exe -Djava.awt.headless=true
 set command=%command% -Xmx%memory% -Djava.rmi.server.hostname=%hostname%
@@ -32,7 +23,4 @@ set command=%command% -classpath %classpath% org.protege.osgi.framework.Launcher
 echo %command%
 
 runas /user:%user% "cmd /k cd %install.dir% & %command%"
-REM %command%
-
-
-
+REM %command%
