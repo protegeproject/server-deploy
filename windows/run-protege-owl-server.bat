3d0338a5b07d7212d099038687dd050183856e0a
Timothy Redmond <tredmond@stanford.edu>
2012-11-07 19:30:12 +0000

    the script seems to be running on the 32 bit machine.

--- windows/run-protege-owl-server.bat
+++ windows/run-protege-owl-server.bat
@@ -1,16 +1,16 @@
+@ECHO OFF
+
 REM User configurable variables
 
-set hostname=localhost
-set memory=700M
-set user=Protege
+set hostname=@hostname@
+set memory=@memory@
+set user=@user@
 set install.dir=C:\Program Files\Protege OWL Server
 
 REM End of user configurable variables section
 
 REM Keep configuring if you like but this should be less likely
 
-REM 
-set classpath='%install.dir%\lib\felix.jar;%install.dir%\lib\ProtegeLauncher.jar'
 set classpath=lib\felix.jar;lib\ProtegeLauncher.jar
 
 set command=java.exe -Djava.awt.headless=true
@@ -20,7 +20,4 @@ set command=%command% -Dorg.protege.owl.server.configuration=metaproject.owl
 set command=%command% -Djava.util.logging.config.file=logging.properties
 set command=%command% -classpath %classpath% org.protege.osgi.framework.Launcher
 
-echo %command%
-
 runas /user:%user% "cmd /k cd %install.dir% & %command%"
-REM %command%
