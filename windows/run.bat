d1dd2eb2567f1ac3a178857fdb87b610e62421c7
Timothy Redmond <tredmond@stanford.edu>
2012-11-07 05:40:07 +0000

    getting close

--- windows/run.bat
+++ windows/run.bat
@@ -1,21 +1,38 @@
 REM User configurable variables
 
+
+
 set hostname=localhost
+
 set memory=700M
+
 set user=Protege
+set install.dir=C:\Program Files\Protege OWL Server
+
+
 
 REM End of user configurable variables section
+
 REM Keep configuring if you like but this should be less likely
+
+
+REM 
+set classpath='%install.dir%\lib\felix.jar;%install.dir%\lib\ProtegeLauncher.jar'
 
 set classpath=lib\felix.jar;lib\ProtegeLauncher.jar
 
 
-runas /user:%user% ^
-    java.exe -Djava.awt.headless=true ^
-         -Xmx%memory% ^
-         -Djava.rmi.server.hostname=%hostname% ^
-         -DentityExpansionLimit=1000000 -Dfile.encoding=UTF-8 ^
-         -Dorg.protege.owl.server.configuration=metaproject.owl ^
-         -Djava.util.logging.config.file=logging.properties ^
-         -classpath %classpath% ^
-         org.protege.osgi.framework.Launcher > ${LOG_DIR}/cmdline 2>&1 &
+set command=java.exe -Djava.awt.headless=true
+set command=%command% -Xmx%memory% -Djava.rmi.server.hostname=%hostname%
+set command=%command% -DentityExpansionLimit=1000000 -Dfile.encoding=UTF-8
+set command=%command% -Dorg.protege.owl.server.configuration=metaproject.owl
+set command=%command% -Djava.util.logging.config.file=logging.properties
+set command=%command% -classpath %classpath% org.protege.osgi.framework.Launcher
+
+echo %command%
+
+runas /user:%user% "cmd /k cd %install.dir% & %command%"
+REM %command%
+
+
+
